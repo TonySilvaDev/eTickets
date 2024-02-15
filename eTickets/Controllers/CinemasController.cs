d54c54b28d9f6fb5cd5f68cda2901377d69fa237
Antonio Silva <antonio.silvamnrl@gmail.com>
2024-02-15 17:12:13 -0600

    Video 25. Adding the cinemas view

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -15,8 +15,8 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var allProducers = await _context.Producers.ToListAsync();
-            return View();
+            var allCinemas = await _context.Cinemas.ToListAsync();
+            return View(allCinemas);
         }
     }
 }
