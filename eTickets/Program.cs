62bfb716f21b66de20eb59a17fba69744b641ddb
Antonio Silva <antonio.silvamnrl@gmail.com>
2024-02-17 20:12:54 -0600

    Video 31. Major dependency-injection lifetimes (Singleton vs Scoped vs Transient)

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,4 +1,5 @@
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+//Services configuration
+builder.Services.AddScoped<IActorService, ActorsService>();
 
 var app = builder.Build();
 
