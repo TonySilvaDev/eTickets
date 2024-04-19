ac31849f908388fc3bd9b0a43dd1f39836772da1
Antonio Silva <antonio.silvamnrl@gmail.com>
2024-04-18 23:21:56 -0600

    Video 76. Listing all orders

--- eTickets/Controllers/OrdersController.cs
+++ eTickets/Controllers/OrdersController.cs
@@ -16,6 +16,15 @@ namespace eTickets.Controllers
             _shoppingCart = shoppingCart;
             _ordersService = ordersService;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            string userId = "";
+            var orders = await _ordersService.GetOrdersByUseridAsync(userId);
+
+            return View(orders);
+        }
+
         public IActionResult ShoppingCart()
         {
             var items = _shoppingCart.GetShoppingCartItems();
