1d726a911ad306996163860035f010b3d1d10bac
Antonio Silva <antonio.silvamnrl@gmail.com>
2024-04-18 20:35:18 -0600

    Video 74. Adding and configuring OrdersService

---
+++ eTickets/Data/Services/IOrdersService.cs
@@ -0,0 +1,10 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Services
+{
+    public interface IOrdersService
+    {
+        Task StoreOrderAsync(List<ShoppingCartItem> items, string uderId, string userEmailAddress);
+        Task<List<Order>> GetOrdersByUseridAsync(string userId);
+    }
+}
