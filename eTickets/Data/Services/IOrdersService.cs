5a2e87e51ab8f4e75e1bf858effbb56be1f51dbc
Antonio Silva <antonio.silvamnrl@gmail.com>
2024-06-08 03:49:21 -0600

    Video 95. Role based Orders view

--- eTickets/Data/Services/IOrdersService.cs
+++ eTickets/Data/Services/IOrdersService.cs
@@ -5,6 +5,6 @@ namespace eTickets.Data.Services
     public interface IOrdersService
     {
         Task StoreOrderAsync(List<ShoppingCartItem> items, string uderId, string userEmailAddress);
-        Task<List<Order>> GetOrdersByUseridAsync(string userId);
+        Task<List<Order>> GetOrdersByUseridAndRoleAsync(string userId, string userRole);
     }
 }
