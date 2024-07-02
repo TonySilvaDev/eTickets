fcf6e710546809a1e113759ff4959c6a438a03fe
Antonio Silva <antonio.silvamnrl@gmail.com>
2024-07-02 16:30:22 -0600

    Video 96. Listing all users

--- eTickets/Controllers/AccountController.cs
+++ eTickets/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using eTickets.Data.ViewModels;
 using eTickets.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace eTickets.Controllers
 {
@@ -19,6 +20,13 @@ namespace eTickets.Controllers
             _signInManager = signInManager;
             _context = context;
         }
+
+        public async Task<ActionResult> Users()
+        {
+            var users = await _context.Users.ToListAsync();
+            return View(users);
+        }
+
         public IActionResult Login() => View(new LoginVM());
 
         [HttpPost]
